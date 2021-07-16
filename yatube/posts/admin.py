55d393a5babee4dc55fad270c40e27d064e3d487
Александр <ruyearn@gmail.com>
2021-07-16 11:31:15 +0300

    fixed admin

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -4,7 +4,7 @@ from .models import Comment, Follow, Group, Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'text', 'pub_date', 'author',)
+    list_display = ('pk', 'text', 'pub_date', 'author')
     search_fields = ('text',)
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
@@ -18,15 +18,15 @@ class GroupAdmin(admin.ModelAdmin):
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('post', 'author', 'text', 'created',)
-    search_fields = ('post', 'author',)
+    list_display = ('post', 'author', 'text', 'created')
+    search_fields = ('post', 'author')
     list_filter = ('created',)
     empty_value_display = '-пусто-'
 
 
 class FollowAdmin(admin.ModelAdmin):
-    list_display = ('user', 'author',)
-    search_fields = ('user', 'author',)
+    list_display = ('user', 'author')
+    search_fields = ('user', 'author')
     empty_value_display = '-пусто-'
 
 
